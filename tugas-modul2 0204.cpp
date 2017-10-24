77f8a117db13696368f14d1d15a89505fffee244
Abdanpicisan <32300417+Abdanpicisan@users.noreply.github.com>
2017-10-24 19:12:15 +0800

    menghitung luas rumah

--- tugas-modul2 0204.cpp
+++ tugas-modul2 0204.cpp
@@ -7,5 +7,9 @@ double Luas_rumah = 42;
 double Lantai_parket = 193*1215;
 double Luas_1box = Lantai_parket*8/1000000;	
 double Jml_box_parket, Biaya_parket, Biaya_pasang, Totalbyr;
+
+cout <<" Luas Rumah : "<<Luas_rumah<<" m2"<<endl;
+cout <<" Luas 1 Box Parket 	: "<<Luas_1box<<"m2"<<endl;
+
 	
 }
