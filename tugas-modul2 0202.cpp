00344b355b11f4f04ded1259369f8d9acf23d9a1
Abdanpicisan <32300417+Abdanpicisan@users.noreply.github.com>
2017-10-24 18:42:52 +0800

    Output bermacam Gaya

--- tugas-modul2 0202.cpp
+++ tugas-modul2 0202.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main(){
 	
-	int nilai0;
-	int nilai1;
-	int nilai2;
-	int nilai3;
+	int nilai=0;
+	int Nilai=1;
+	int NILAI=2;
+	int niLAI=3;
 	
 	double angka(10.5),Angka,ANGKA(3.14);
 	int a(10),b(7),c(12);
 	
+	cout<< nilai<<" : "<<Nilai<<" | "<<NILAI<<" "<<niLAI<<endl;
+	cout<<angka<<"\n";
+	cout<<Angka<<endl<<ANGKA;
 }
